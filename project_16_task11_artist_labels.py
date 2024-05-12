ee00e794e863267908b54d13a91ea2bbdd80f184
jannehurnanen <98103353+jannehurnanen@users.noreply.github.com>
2024-05-12 13:49:32 +0300

    Update project_16_task11_artist_labels.py

--- project_16_task11_artist_labels.py
+++ project_16_task11_artist_labels.py
@@ -1,30 +1,44 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load data
 data = pd.read_csv('user_taggedartists.dat', sep='\t')
 artists = pd.read_csv('artists.dat', sep='\t')
 
-# Merge data to get artist names
-merged_data = pd.merge(data, artists[['id', 'name']], left_on='artistID', right_on='id', how='left')
+merged_data = pd.merge(
+    data,
+    artists[['id', 'name']],
+    left_on='artistID',
+    right_on='id',
+    how='left'
+)
 
-# Task 10 implemented
+artist_popularity = merged_data \
+                    .groupby(['artistID', 'name']) \
+                    .size() \
+                    .reset_index(name='popularity')
 
-# Calculate artist popularity
-artist_popularity = merged_data.groupby(['artistID', 'name']).size().reset_index(name='popularity')
-artist_popularity = artist_popularity.sort_values(by='popularity', ascending=False)
+for year in range(2005, 2012):
+    plt.figure()
+    yearly_data = merged_data[merged_data['year'] == year]
+    yearly_popularity = yearly_data \
+                    .groupby(['artistID', 'name']) \
+                    .size() \
+                    .reset_index(name='popularity')
+    yearly_popularity = yearly_popularity \
+                    .merge(artist_popularity, 
+                           on=['artistID', 'name'], 
+                           how='inner')
+    yearly_popularity = yearly_popularity \
+                    .sort_values(by='popularity_x', ascending=False) \
+                    .head(10)
+    
+    plt.bar(yearly_popularity['name'], 
+        yearly_popularity['popularity_x'], 
+        color='skyblue')
+    plt.xlabel('Artist')
+    plt.ylabel('Popularity')
+    plt.title(f'Top 10 Popular Artists in {year}')
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
 
-# Select top 10 popular artists
-top_10_popular_artists = artist_popularity.head(10)
-
-# Plot popularity for each artist
-for artistID, artist_name in zip(top_10_popular_artists['artistID'], top_10_popular_artists['name']):
-    artist_data = merged_data[merged_data['artistID'] == artistID]
-    artist_yearly_popularity = artist_data.groupby('year').size()
-    artist_yearly_popularity.plot(label=f'{artist_name}')
-
-plt.xlabel('Year')
-plt.ylabel('Popularity')
-plt.title('Yearly Popularity of Top 10 Artists')
-plt.legend()
 plt.show()
